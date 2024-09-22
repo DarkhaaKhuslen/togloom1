f4838ecb738ec70d166b2d4ec5083313dea748e4
DarkhaaKhuslen <167779315+DarkhaaKhuslen@users.noreply.github.com>
2024-09-22 17:09:23 +0800

    duus

---
+++ app.js
@@ -0,0 +1,29 @@
+alert("Ta sagsan bombog togloj bailaa");
+alert("Ta 6 onoo awbal hojih bolno")
+var choice=prompt("2 steps/mid range");
+if(choice=="2 steps"  ||  choice=="2 steps");{
+    alert("1 point")
+    var choice1=prompt("jelly/layup");
+    if(choice1=="jelly" || choice1=="jelly"){
+        alert("1 point")
+    }else if(choice1=="layup"){
+        alert("block")
+    }
+    var choice2=prompt("2 point arc/fadeaway")
+    if(choice2=="2 point arc" || choice2=="2 point arc"){
+        alert("2 point")
+    }else if(choice2=="fadeaway"){
+        alert("block")
+    }
+    var choice3=prompt("defense/attack");
+    if(choice3=="defense" || choice3=="defense");{
+        alert("good defense")
+    }
+    var choice4=prompt("2 point arc/euro steps")
+    if(choice4=="2 point arc" || choice4=="2 point arc"){
+        alert("2 point")
+    }else if(choice4=="euro steps"){
+        alert("block")
+    }
+    alert("ta hojloo 6 onoonoo zoruutei")
+}
